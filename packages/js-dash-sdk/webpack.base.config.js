06622ed4be1e5bdcd5de307f3e6ec4973dca6cb0
Jon Kindel <jon@blackcircletechnologies.com>
2022-10-23 21:27:11 -0500

    add snaps webpack plugin (WIP)

--- packages/js-dash-sdk/webpack.base.config.js
+++ packages/js-dash-sdk/webpack.base.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const SnapsWebpackPlugin = require('@metamask/snaps-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
 
 const baseConfig = {
@@ -46,6 +47,31 @@ const baseConfig = {
     },
   },
   plugins: [
+    new SnapsWebpackPlugin({
+      /**
+       * Whether to strip all comments from the bundle.
+       */
+      stripComments: true,
+    
+      /**
+       * Whether to evaluate the bundle with SES, to ensure SES compatibility.
+       */
+      eval: true,
+    
+      /**
+       * The path to the Snap manifest file. If set, it will be checked and automatically updated with
+       * the bundle's hash, if `writeManifest` is enabled. Defaults to `snap/manifest.json` in the
+       * current working directory.
+       */
+      manifestPath: './snap.manifest.json',
+    
+      /**
+       * Whether to write the updated Snap manifest file to disk. If `manifestPath` is not set, this
+       * option has no effect. If this is disabled, an error will be thrown if the manifest file is
+       * invalid.
+       */
+      writeManifest: true,
+    }),
     new webpack.ProvidePlugin({
       Buffer: [require.resolve('buffer/'), 'Buffer'],
       process: require.resolve('process/browser'),
